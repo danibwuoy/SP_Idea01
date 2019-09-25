cf7eaf54ae8bd6928a47725a2a7a7e507579d42d
Daniel <dani.020196@gmail.com>
2019-09-26 01:16:37 +0530

    Assets Script prototype for Player and Planet

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public float gravity = -10;
+    [SerializeField]
+    private float bodyRotationSpeed = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ApplyGravity(Transform body)
+    {
+        Vector3 gravityDirection = (body.position - transform.position).normalized;
+        Rigidbody rb = body.GetComponent<Rigidbody>();
+        if (rb == null)
+            return;
+        rb.AddForce(gravityDirection * gravity);
+        Quaternion targetRotation = Quaternion.FromToRotation(body.up, gravityDirection) * body.rotation;
+        body.rotation = Quaternion.Slerp(body.rotation,targetRotation,bodyRotationSpeed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Planet planet;
+    [SerializeField]
+    private float speed = 15f;
+    private Rigidbody rigidbody;
+    private Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        if (!rigidbody)
+        {
+            Debug.LogError("Please Attach a RigidBody to continue WTF");
+        }
+        rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        rigidbody.useGravity = false;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + transform.TransformDirection(moveDirection)*speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        planet.ApplyGravity(transform);
+    }
+}
